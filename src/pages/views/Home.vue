de1b8e12097e6082f0ba715f7cd142464c095671
Jacky LIM <jacky.lim@edu.itescia.fr>
2019-04-16 19:28:28 +0200

    Feat: Data in server now + implemented services

--- src/pages/views/Home.vue
+++ src/pages/views/Home.vue
@@ -59,7 +59,7 @@
 </template>
 
 <script>
-import PEOPLE from '../_static/people.js';
+import peopleService from '../services/PeopleService.js';
 
 
 export default {  
@@ -69,11 +69,17 @@ export default {
     }
   },
   created:function(){
-    this.person=PEOPLE[0];
+    peopleService
+    .fetch()
+    .then(people=>this.person=people[0])
+    .catch(console.log);
   },
   methods:{
     random: function(){
-      this.person=PEOPLE[Math.floor(Math.random()*PEOPLE.length)];
+      peopleService
+      .fetchRandom()
+      .then(person =>this.person = person)
+      .catch(console.log);
     }
   }
 }
